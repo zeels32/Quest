b73677e0a1bef93f0a621af587d8720e7a4e5ec9
Zeel <zeel.chetan@anywhere.co>
2025-06-18 12:22:44 +0530

    refactor: added quest title on the home page

--- app/src/main/java/com/quests/demo/presentation/ui/home/compose/HomeScreen.kt
+++ app/src/main/java/com/quests/demo/presentation/ui/home/compose/HomeScreen.kt
@@ -5,11 +5,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.quests.demo.events.presentation.events.components.TitleBar
 
 @Composable
 fun HomeScreen(
@@ -18,25 +20,36 @@ fun HomeScreen(
 ) {
 
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp) // Add padding for better spacing,
-    ) {
-        Button(
-            modifier = Modifier.fillMaxWidth(),
-            onClick = navigateToEvent
-        ) {
-            Text(text = "Quest 1")
-        }
+    Scaffold(
+        topBar = {
+            TitleBar(
+                text = "Quests",
+                onBack = {}
+            )
+        },
+        content = { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+                    .padding(16.dp) // Add padding for better spacing,
+            ) {
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = navigateToEvent
+                ) {
+                    Text(text = "Quest 1")
+                }
 
-        Button(
-            onClick = navigateToProduct,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text(text = "Quest 2")
+                Button(
+                    onClick = navigateToProduct,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Quest 2")
+                }
+            }
         }
-    }
+    )
 
 }
 
